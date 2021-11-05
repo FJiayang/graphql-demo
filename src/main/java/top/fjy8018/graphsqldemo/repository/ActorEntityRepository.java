2e3ce1a8ef66d27f9cba42ae7135e414623ad581
fujiayang <fujiayang@yingmi.cn>
2021-11-05 16:14:27 +0800

    补充依赖

--- src/main/java/top/fjy8018/graphsqldemo/repository/ActorEntityRepository.java
+++ src/main/java/top/fjy8018/graphsqldemo/repository/ActorEntityRepository.java
@@ -3,7 +3,7 @@ package top.fjy8018.graphsqldemo.repository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.graphql.data.GraphQlRepository;
-import top.fjy8018.graphsqldemo.entity.CityEntity;
+import top.fjy8018.graphsqldemo.entity.ActorEntity;
 
 /**
  * TODO:
@@ -12,6 +12,6 @@ import top.fjy8018.graphsqldemo.entity.CityEntity;
  * @date 2021/11/5 10:35
  */
 @GraphQlRepository
-public interface CityEntityRepositoy extends
-        CrudRepository<CityEntity, Integer>, QuerydslPredicateExecutor<CityEntity> {
+public interface ActorEntityRepository extends
+        CrudRepository<ActorEntity, Integer>, QuerydslPredicateExecutor<ActorEntity> {
 }
